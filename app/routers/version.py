4b227b9710372fc9f6b241e2418de88ce5923b9e
gokceyetiserp <gokce@pluraf.com>
2024-10-04 15:46:17 +0200

    Add serial_number to version endpoint

--- app/routers/version.py
+++ app/routers/version.py
@@ -15,8 +15,20 @@ def read_version_from_file(file_path: str) -> Dict[str, str]:
             api_versions[api_name] = version 
     return api_versions
 
+def read_serial_number_from_file(file_path: str):
+    with open(file_path, "r") as file:
+        serial_number = file.read()
+    return serial_number
+
 
 @router.get("/")
 async def api_version_get():
     api_versions = read_version_from_file("./api_versions.txt")
-    return api_versions
+    serial_number = read_serial_number_from_file("./serial_number.txt")
+
+    rv = {
+        "API VERSIONS" : api_versions,
+        "SERIAL_NUMBER" : serial_number
+    }
+    
+    return rv
