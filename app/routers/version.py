2518d04aed7e01141e85c978ac4ebaf8787d3e5b
gokceyetiserp <gokce@pluraf.com>
2024-10-04 15:54:26 +0200

    Refactor to read versions from related ends

--- app/routers/version.py
+++ app/routers/version.py
@@ -1,8 +1,39 @@
 from fastapi import APIRouter
-
+import zmq
 from typing import Dict
 
 router = APIRouter()
+context = zmq.Context()  # Should be terminated when application exits
+
+def get_version_from_zmq(address: str) -> str:
+    socket = context.socket(zmq.REQ)
+    try:
+        socket.connect(address)
+        socket.send_string("api_version")
+        socket.setsockopt(zmq.RCVTIMEO, 5000)
+        version = socket.recv_string()
+    except zmq.error.ZMQError as e:
+        print(f"Error retrieving version from {address}: {e}")
+        return "unknown"
+    finally:
+        socket.close()
+    return version
+
+
+def parse_api_versions() -> Dict[str, str]:
+    api_versions = {}
+    try:
+        api_versions["M-BROKER-C"] = get_version_from_zmq("ipc:///tmp/mqbc-zmq.sock")
+    except Exception as e:
+        api_versions["M-BROKER-C"] = "unknown"
+        print(f"Failed to retrieve version for M-BROKER-C: {e}")
+    try:
+        api_versions["M2E-BRIDGE"] = get_version_from_zmq("ipc:///tmp/m2eb-zmq.sock")
+    except Exception as e:
+        api_versions["M2E-BRIDGE"] = "unknown"
+        print(f"Failed to retrieve version for M2E-BRIDGE: {e}")
+    return api_versions
+
 
 def read_version_from_file(file_path: str) -> Dict[str, str]:
     api_versions = {}
@@ -10,10 +41,9 @@ def read_version_from_file(file_path: str) -> Dict[str, str]:
     with open(file_path, "r") as file:
         for line in file:
             api_name, version = line.split("=")
-            api_name = api_name.strip().replace("API_VERSION", "").strip()
-            version = version.strip()
-            api_versions[api_name] = version 
+            api_versions[api_name.strip()] = version.strip()
     return api_versions
+
 
 def read_serial_number_from_file(file_path: str):
     with open(file_path, "r") as file:
@@ -23,7 +53,10 @@ def read_serial_number_from_file(file_path: str):
 
 @router.get("/")
 async def api_version_get():
-    api_versions = read_version_from_file("./api_versions.txt")
+    gnode_api_version = read_version_from_file("./api_versions.txt")
+    mbc_m2eb_api_versions = parse_api_versions()
+    api_versions = {**gnode_api_version, **mbc_m2eb_api_versions}
+
     serial_number = read_serial_number_from_file("./serial_number.txt")
 
     rv = {
