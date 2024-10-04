6ad3f36a19cbfc12b0e4e8b3bfe7ec0e4653c582
gokceyetiserp <gokce@pluraf.com>
2024-10-04 14:32:26 +0200

    Add set gnode_time functionality

---
+++ app/components/gnode_time.py
@@ -0,0 +1,60 @@
+from fastapi import HTTPException
+
+import subprocess
+
+
+def delete_old_ntp_servers():
+    try:
+        ntp_sources = subprocess.run(
+            ['sudo', 'chronyc', 'ntpdata'], 
+            check=True, capture_output=True, text=True
+        )
+        ntp_servers = []
+        for line in ntp_sources.stdout.splitlines():
+            if line.startswith("Remote address"):
+                server = line.split(":")[1].strip()
+                ntp_servers.append(server)
+        for server in ntp_servers:
+            subprocess.run(['sudo', 'chronyc', 'delete', server], check=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code = 500, detail = f"Failed to remove old servers: {e}")
+
+
+def set_time_manually(date_time, time_zone):
+    try:
+        subprocess.run(['sudo', 'timedatectl', 'set-ntp', 'false'], check=True)
+        subprocess.run(['sudo', 'timedatectl', 'set-local-rtc', '0'], check=True)
+        subprocess.run(['sudo', 'timedatectl', 'set-timezone', time_zone], check=True)
+        subprocess.run(['sudo', 'date', '--set', date_time], check=True)
+        subprocess.run(['sudo', 'timedatectl', 'set-local-rtc', '1'], check=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code = 500, detail = f"Failed to set system time: {e}")
+
+
+def set_time_automatically(ntp_server, time_zone):
+    try:
+        subprocess.run(['sudo', 'timedatectl', 'set-local-rtc', '0'], check=True)
+        subprocess.run(['sudo', 'timedatectl', 'set-timezone', time_zone], check=True)
+        subprocess.run(['sudo', 'systemctl', 'restart', 'chrony'], check=True)
+        delete_old_ntp_servers()
+        subprocess.run(['sudo', 'chronyc', 'add', 'server', ntp_server, 'iburst'], check=True)
+        subprocess.run(['sudo', 'chronyc', 'makestep'], check=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code = 500, detail = f"Failed to sync system time: {e}")
+
+
+def set_gnode_time(user_input):
+    if not user_input.get('automatic'):
+        date = user_input.get('date')
+        time = user_input.get('time')
+        time_zone = user_input.get('timezone')
+        if not date or not time or not time_zone:
+            raise HTTPException(status_code = 422, detail = "Missing required field!")
+        date_time = f"{date} {time}"
+        set_time_manually(date_time, time_zone)
+    else:
+        time_zone = user_input.get('timezone')
+        ntp_server = user_input.get('ntp_server')
+        if not time_zone:
+            raise HTTPException(status_code = 422, detail = "Missing required field!")
+        set_time_automatically(ntp_server, time_zone)
--- app/routers/settings.py
+++ app/routers/settings.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 from typing import Any
 
 from app.routers import authentication
-
+from app.components import gnode_time
 
 router = APIRouter()
 
@@ -26,5 +26,9 @@ async def settings_get(_: str = Depends(authentication.validate_jwt)):
 async def settings_put(settings: dict[str, Any], _: str = Depends(authentication.validate_jwt)):
     a = settings["allow_anonymous"]
     socket.send(b'\x01' if a else b'\x00')
+
+    if "gnode_time" in settings:
+        gnode_time.set_gnode_time(settings["gnode_time"])
+
     socket.recv()
     return Response(status_code=200)
