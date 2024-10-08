6b016cda86ebb9c7a762f0c39d9050699c0e23fe
Konstantin Tyurin <konstantin@pluraf.com>
2024-10-08 17:49:45 +0200

    Align env vars and do not hang in zeromq no response

--- app/routers/settings.py
+++ app/routers/settings.py
@@ -13,22 +13,31 @@ router = APIRouter()
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("ipc:///tmp/mqbc-zmq.sock")
+socket.setsockopt(zmq.RCVTIMEO, 2000)
 
 
 @router.get("/")
 async def settings_get(_: str = Depends(authentication.validate_jwt)):
-    socket.send_string('')
-    message = socket.recv()
+    try:
+        socket.send_string('')
+        message = socket.recv()
+    except zmq.error.ZMQError:
+        message = b"\x00"
     return JSONResponse(content={"allow_anonymous": bool(message[0])})
 
 
 @router.put("/")
 async def settings_put(settings: dict[str, Any], _: str = Depends(authentication.validate_jwt)):
-    a = settings["allow_anonymous"]
-    socket.send(b'\x01' if a else b'\x00')
+    v = settings.get("allow_anonymous")
+    if v is not None:
+        try:
+            socket.send(b'\x01' if v else b'\x00')
+            socket.recv()
+        except zmq.error.ZMQError:
+            pass
 
-    if "gnode_time" in settings:
-        gnode_time.set_gnode_time(settings["gnode_time"])
+    v = settings.get("gnode_time")
+    if v is not None:
+        gnode_time.set_gnode_time(v)
 
-    socket.recv()
     return Response(status_code=200)
