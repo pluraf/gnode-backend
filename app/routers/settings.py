1d0a95b2b77b9a7ee4570ea1d8d1e4924dbfe0f5
gokceyetiserp <gokce@pluraf.com>
2024-10-28 10:36:28 +0100

    Add get api endpoint for gnode time

--- app/routers/settings.py
+++ app/routers/settings.py
@@ -1,8 +1,10 @@
 import zmq
 
-from fastapi import APIRouter, Response, Depends
+from fastapi import APIRouter, Response, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from typing import Any
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
 from app.routers import authentication
 from app.components import gnode_time
@@ -17,6 +19,8 @@ async def settings_get(_: str = Depends(authentication.validate_jwt)):
     socket.connect("ipc:///tmp/mqbc-zmq.sock")
     socket.setsockopt(zmq.RCVTIMEO, 1000)
 
+    response = {}
+
     try:
         socket.send_string('')
         message = socket.recv()
@@ -24,7 +28,19 @@ async def settings_get(_: str = Depends(authentication.validate_jwt)):
         message = b"\x00"
     finally:
         socket.close()
-    return JSONResponse(content={"allow_anonymous": bool(message[0])})
+    response["allow_anonymous"] = bool(message[0])
+
+    current_timestamp = int(datetime.now(timezone.utc).timestamp())
+    with open("/etc/timezone", "r") as tz:
+        current_timezone = tz.read().strip()
+    iso_8601_utc = datetime.now(ZoneInfo(current_timezone)).strftime("%Y-%m-%dT%H:%M:%SZ")
+    response["time"] = {
+                        "timestamp": current_timestamp,
+                        "time": iso_8601_utc,
+                        "timezone": current_timezone
+                        }
+
+    return JSONResponse(content=response)
 
 
 @router.put("/")
