8b46383274821087cd6370322a0be67e418b6b93
revathy <revathy@pluraf.com>
2024-11-21 12:04:44 +0100

    Moved settings table initialization to startup

--- app/components/settings.py
+++ app/components/settings.py
@@ -5,20 +5,13 @@ from sqlalchemy.orm import sessionmaker
 
 from app.models.settings import SettingsModel
 from app.database_setup import default_engine
-from app.components.status import get_service_status, ServiceStatus
 import app.settings as app_settings
+from app.utils import get_mode, GNodeMode
 
 class Settings:
     def __init__(self):
         session = sessionmaker(bind=default_engine)()
         self._settings = session.query(SettingsModel).first()
-        if self._settings is None:
-            gcloud_status = \
-                (get_service_status(app_settings.GCLOUD_SERVICE_NAME) == ServiceStatus.RUNNING)
-            self._settings = SettingsModel(gcloud = gcloud_status)
-            session.add(self._settings)
-            session.commit()
-            self._settings = session.query(SettingsModel).first()
         session.close()
 
     @property
@@ -62,3 +55,23 @@ class Settings:
             raise RuntimeError("Error changing g-cloud status")
         finally:
             session.close()
+
+def init_settings_table():
+    session = sessionmaker(bind=default_engine)()
+    settings = session.query(SettingsModel).first()
+    if settings is None:
+        session.add(SettingsModel())
+        session.commit()
+        settings = session.query(SettingsModel).first()
+        session.close()
+        if get_mode() == GNodeMode.PHYSICAL :
+            try:
+                if (settings.gcloud):
+                    subprocess.run(["sudo", "/bin/systemctl", "start", app_settings.GCLOUD_SERVICE_NAME], check=True)
+                else:
+                    subprocess.run(["sudo", "/bin/systemctl", "stop", app_settings.GCLOUD_SERVICE_NAME], check=True)
+            except subprocess.CalledProcessError:
+                # Letting execution continue in case of error
+                # TODO: Change print to log later
+                print("Error initializing gcloud settings!")
+
