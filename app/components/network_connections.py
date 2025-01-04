958480f82548ee5137db31fd1ebc8dab427a346c
Konstantin Tyurin <konstantin@pluraf.com>
2025-01-04 17:50:32 +0100

    Apply license

--- app/components/network_connections.py
+++ app/components/network_connections.py
@@ -1,3 +1,20 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# Copyright (c) 2024 Pluraf Embedded AB <code@pluraf.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from fastapi import HTTPException
 
 import subprocess
@@ -68,7 +85,7 @@ def is_valid_gateway(gateway, network_ip, subnet_mask):
         # Check if the gateway is within the network range but not the network address
         gateway_ip = ipaddress.IPv4Address(gateway)
         return (
-            gateway_ip in network 
+            gateway_ip in network
             and gateway_ip != network.network_address
             and gateway_ip != network.broadcast_address
         )
@@ -262,7 +279,7 @@ def set_network_settings(user_input):
     # Accepts input of format:
     # {
     #    wifi_state:
-    #    type: 
+    #    type:
     #    ssid:
     #    password:
     #    ipv4_method:
